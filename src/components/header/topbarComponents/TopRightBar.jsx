f6443d0c5e953a6c93e0f02f20314504befe7ad9
Nazmul Alam <nazmulcse08@gmail.com>
2025-05-14 19:16:11 +0600

    Top Bar Done

--- src/components/header/topbarComponents/TopRightBar.jsx
+++ src/components/header/topbarComponents/TopRightBar.jsx
@@ -1,19 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { TfiAngleDown } from "react-icons/tfi";
+import { Link } from "react-router-dom";
 
 const TopRightBar = () => {
+  const [selectedCountry, setSelectedCountey] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const countries = [
+    {
+      name: "United States",
+      value: "US",
+      flag: "https://flagcdn.com/16x12/us.png",
+    },
+    { name: "Canada", value: "CA", flag: "https://flagcdn.com/16x12/ca.png" },
+    {
+      name: "United Kingdom",
+      value: "GB",
+      flag: "https://flagcdn.com/16x12/gb.png",
+    },
+    {
+      name: "Australia",
+      value: "AU",
+      flag: "https://flagcdn.com/16x12/au.png",
+    },
+    { name: "Germany", value: "DE", flag: "https://flagcdn.com/16x12/de.png" },
+    { name: "France", value: "FR", flag: "https://flagcdn.com/16x12/fr.png" },
+  ];
+
+  const handleSelect = (country) => {
+    setSelectedCountey(country);
+    isOpen(false);
+  };
+
   return (
     <>
       <div className="flex justify-end items-center gap-[49px]">
-        <div>USD</div>
-        <div className="relaltive after:content-[''] after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:right-[212px] after:top-[50%] after:-translate-y-1/2 before:content-[''] before:absolute before:w-[1px] before:h-[32px] before:bg-[#BFBFBF] before:right-[112px] before:top-[50%] before:-translate-y-1/2">
-          <select name="" id="">
-            <option value="">asd</option>
-            <option value="">dfg</option>
-            <option value="">wee</option>
-            <option value="">qwe</option>
+        <div>
+          <select name="currency">
+            <option value="USD">USD</option>
+            <option value="BDT">BDT</option>
           </select>
         </div>
-        <div>social Media</div>
+
+        <div className="relaltive after:content-[''] after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:right-[295px] after:top-[50%] after:-translate-y-1/2 before:content-[''] before:absolute before:w-[1px] before:h-[32px] before:bg-[#BFBFBF] before:right-[112px] before:top-[50%] before:-translate-y-1/2">
+          <select
+            className="w-[150px] hidden"
+            name="country"
+            value={selectedCountry?.value}
+            onChange={(e) => {
+              const country = countries.find((c) => c.value === e.target.value);
+              setSelectedCountey(country);
+            }}
+          >
+            {countries.map((country, index) => (
+              <option key={index} value={country.value}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+          {/* Custom Dropdown */}
+          <div
+            className="w-[150px] p-2 cursor-pointer flex items-center"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            {selectedCountry ? (
+              <>
+                <img
+                  src={selectedCountry?.flag}
+                  alt={selectedCountry?.name}
+                  className="w-5 h-4 mr-2"
+                />
+                <span className="mr-6">{selectedCountry?.name}</span>
+                <TfiAngleDown />
+              </>
+            ) : (
+              <>
+                <span className="mr-4">Select Country</span>
+                <TfiAngleDown />
+              </>
+            )}
+          </div>
+
+          {/* Option List */}
+          {isOpen && (
+            <ul className="absolute border border-gray-300 bg-white shadow-lg z-10">
+              {countries.map((country, index) => (
+                <li
+                  key={index}
+                  className="w-[150px] flex items-center gap-2 p-2 hover:bg-gray-200 cursor-pointer"
+                  onClick={() => handleSelect(country)}
+                >
+                  <img
+                    src={country?.flag}
+                    alt={country?.name}
+                    className="w-5 h-4 mr-2"
+                  />
+                  {country?.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="flex gap-4">
+          <Link to="#">
+            <FaFacebookF />
+          </Link>
+          <Link to="#">
+            <FaTwitter />
+          </Link>
+          <Link to="#">
+            <FaInstagram />
+          </Link>
+        </div>
       </div>
     </>
   );
