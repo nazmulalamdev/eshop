a1c3c54779b23c21468a51499968935f77c08dba
Nazmul Alam <nazmulcse08@gmail.com>
2025-05-16 12:19:51 +0600

    all update

--- src/components/header/BottomBar.jsx
+++ src/components/header/BottomBar.jsx
@@ -1,8 +1,29 @@
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Container from "../commonLayout/Container";
+import { useEffect, useRef, useState } from "react";
+import { FaChevronDown } from "react-icons/fa6";
 
 const BottomBar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutSide = (event) => {
+      // console.log(dropdownRef.current.contains(event.target));
+      // console.log(event.target);
+
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutSide);
+  }, []);
+
   return (
     <>
       <div className="bg-[#FF624C]">
@@ -18,8 +39,29 @@ const BottomBar = () => {
                     <FaBars /> All Categories
                   </Link>
                 </li>
-                <li>
-                  <Link to={"/product-list"}>Products</Link>
+                <li ref={dropdownRef} className="relative">
+                  <button onClick={handleDropdown} className="cursor-pointer">
+                    Products{" "}
+                    <FaChevronDown className="absolute top-1 -right-6" />
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="absolute z-10 mt-2 w-36 bg-white rounded shadow-lg">
+                      <ul className="py-2 font-['Montserrat'] font-normal text-base leading-6 text-black">
+                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                          Products1
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                          Products2
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                          Products3
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                          Products4
+                        </li>
+                      </ul>
+                    </div>
+                  )}
                 </li>
                 <li>
                   <Link to={"/blog"}>Blog</Link>
