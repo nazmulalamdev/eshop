71cc24e0951e56ac012d01899c42c6e7fe48c9f7
Nazmul Alam <nazmulcse08@gmail.com>
2025-05-18 18:12:49 +0600

    slider done

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -1,18 +1,112 @@
 import React from "react";
 import ProductLayout from "./commonLayout/ProductLayout";
 import Container from "./commonLayout/Container";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
 const FeaturedProduct = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
+
   return (
     <>
       <Container>
-        <div className="flex gap-1">
-          <ProductLayout />
-          <ProductLayout />
-          <ProductLayout />
-          <ProductLayout />
-          <ProductLayout />
-        </div>
+        <Slider {...settings}>
+          {/* <div className="flex gap-1"> */}
+          <ProductLayout
+            percentTag={true}
+            category="laptop"
+            title="S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+            rating="5"
+            totalRating="80"
+            price="1,199.00"
+            border={true}
+            bg="transparent"
+            stock={true}
+            stockAmount="50"
+          />
+          <ProductLayout
+            percentTag={false}
+            category="phone"
+            title="S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+            rating="4"
+            totalRating="100"
+            price="1,199.00"
+            border={true}
+            bg="transparent"
+            stock={false}
+            stockAmount="40"
+          />
+          <ProductLayout
+            percentTag={false}
+            category="audio"
+            title="S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+            rating="3"
+            totalRating="64"
+            price="1,199.00"
+            border={true}
+            bg="transparent"
+            stock={true}
+            stockAmount="60"
+          />
+          <ProductLayout
+            percentTag={false}
+            category="camera"
+            title="S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+            rating="2"
+            totalRating="130"
+            price="1,199.00"
+            border={true}
+            bg="transparent"
+            stock={false}
+            stockAmount="10"
+          />
+          <ProductLayout
+            percentTag={false}
+            category="television"
+            title="S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+            rating="4"
+            totalRating="20"
+            price="1,199.00"
+            border={true}
+            bg="transparent"
+            stock={true}
+            stockAmount="20"
+          />
+          {/* </div> */}
+        </Slider>
       </Container>
     </>
   );
--- src/components/commonLayout/ProductLayout.jsx
+++ src/components/commonLayout/ProductLayout.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 
-const ProductLayout = () => {
+const ProductLayout = ({
+  percentTag,
+  category,
+  title,
+  rating,
+  totalRating,
+  price,
+  border,
+  bg,
+  stock,
+  stockAmount,
+}) => {
+  const [ratingValue, setReatingValue] = useState(
+    new Array(+rating).fill(rating)
+  );
+  console.log(ratingValue);
+
   return (
     <>
-      <div className="border border-transparent hover:border-[#C3C3C3] duration-300 p-6 group rounded-lg mb-20">
+      <div
+        style={{ background: bg }}
+        className="border border-transparent hover:border-[#C3C3C3] duration-300 p-6 group rounded-lg mb-20"
+      >
         <div className="relative">
-          <img src="images/productimg.png" alt="productimg" />
-          <div className="absolute top-[-8px] right-[-9px] py-[7px] px-[20px] bg-[#FF624C] rounded-[5px] font-['Montserrat'] font-bold text-base text-[#FFFFFF]">
-            50%
-          </div>
+          <img
+            className="w-full"
+            src="images/productimg.png"
+            alt="productimg"
+          />
+          {percentTag && (
+            <div className="absolute top-[-8px] right-[-9px] py-[7px] px-[20px] bg-[#FF624C] rounded-[5px] font-['Montserrat'] font-bold text-base text-[#FFFFFF]">
+              50%
+            </div>
+          )}
           <div className="flex items-center absolute bottom-[6px] left-[48%] -translate-x-1/2 gap-5 scale-0 group-hover:scale-100 duration-300">
             <div className="w-[50px] h-[50px] bg-white rounded-full border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:text-white hover:bg-[#FF624C] duration-300 cursor-pointer">
               <MdOutlineShoppingCart />
@@ -27,24 +52,31 @@ const ProductLayout = () => {
         </div>
         <div>
           <p className="font-['Montserrat'] font-normal text-sm text-['#303030'] leading-5 uppercase tracking-[5px] mt-12 mb-4">
-            LAPTOP
+            {category}
           </p>
           <h3 className="font-['Poppins'] font-semibold text-xl text-['#303030'] leading-[30px] group-hover:text-[#FF624C] group-hover:underline duration-300">
-            S21 Laptop Ultra HD LED Screen Feature 2023 ...
+            {title}
           </h3>
           <div className="flex items-center gap-2 text-[#FED550] mt-1 mb-6">
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
+            {ratingValue.map((item, index) => (
+              <FaStar key={index} />
+            ))}
             <span className="font-['Montserrat'] font-normal text-base text-[#303030]">
-              ( 100 )
+              ( {totalRating} )
             </span>
           </div>
           <p className="font-['Poppins'] font-semibold text-2xl text-['#303030']">
-            $1,199.00
+            ${price}
           </p>
+          {stock && (
+            <div className="w-full h-[30px] bg-[#E0E0E0] rounded-[25px] mt-[32px] relative">
+              <div className="w-1/2 h-[30px] bg-[#303030] rounded-[25px]">
+                <p className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] text-white uppercase font-['Montserrat'] font-bold text-base">
+                  {stockAmount} AVAILABLE
+                </p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
